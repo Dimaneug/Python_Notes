6d8e052f21defd974453c5be5c0d8c5e3ad3e93d
Dimaneug <dimaneug@gmail.com>
2023-09-23 01:09:34 +0300

    Added implementation of add_note

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import csv
+from datetime import datetime
 
 fields = ['ID', 'Title', 'Body', 'Date']
+last_id = 1
 
 def add_note():
-    pass
+    title = input("Введите заголовок заметки: ")
+    body = input("Введите тело заметки: ")
+    note = {
+        'ID' : last_id,
+        'Title' : title,
+        'Body' : body,
+        'Datetime' : datetime.now()
+    }
+    save_note(note)
+
 
 def save_note(note: dict):
     with open('notes.csv', 'a') as file:
